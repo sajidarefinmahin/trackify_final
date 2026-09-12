f9801444430424954d9f62b50ecbc76cb5df7c48
rubaidul09 <rubaidulmuaz09@gmail.com>
2026-09-12 18:45:00 +0600

    Create BottomNavBar custom widget

---
+++ lib/widgets/bottom_nav.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class BottomNav extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+
+  const BottomNav({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: onTap,
+      type: BottomNavigationBarType.fixed,
+      selectedItemColor: Colors.green,
+      unselectedItemColor: Colors.grey,
