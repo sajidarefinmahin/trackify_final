342908d0e4cabee9a06a371beb0ed5f2f589b69c
sajidarefinmahin <sajidarefinmahin@gmail.com>
2026-09-23 10:30:00 +0600

    Create AuthGate widget for user authentication flow

---
+++ lib/screens/auth_gate.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import '../main.dart';
+import 'login_screen.dart';
+
+class AuthGate extends StatelessWidget {
+  final Widget? home;
+
+  const AuthGate({
+    super.key,
+    this.home,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState ==
+            ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
